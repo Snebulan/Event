45448ac4769cd872782a3d57805c61f3b87464d0
Snebulan <33496560+Snebulan@users.noreply.github.com>
2019-04-17 12:31:08 +0200

    Added functionality to JoinEvent

--- Event/Event/Models/JoinEvent.cs
+++ Event/Event/Models/JoinEvent.cs
@@ -12,5 +12,35 @@ namespace Event.Models
 
         public virtual Event Event { get; set; }
         public virtual User User { get; set; }
+
+        public void SignUp(User user, Event chosenEvent)
+        {
+            var _context = new EventContext();
+            var thisUser = _context.User.Find(user.Id);
+            var thisEvent = _context.Event.Find(chosenEvent.Id);
+
+            JoinEvent join = new JoinEvent
+            {
+                UserId = thisUser.Id,
+                EventId = thisEvent.Id,
+                Date = DateTime.Now,
+                Event = thisEvent,
+                User = thisUser,
+            };
+
+            _context.JoinEvent.Add(join);
+            _context.SaveChanges();
+        }
+
+        public void SignOff(User user, Event chosenEvent)
+        {
+            var _context = new EventContext();
+            var thisUser = _context.User.Find(user.Id);
+            var thisEvent = _context.Event.Find(chosenEvent.Id);
+            var join = _context.JoinEvent.Find(user.Id, chosenEvent.Id);
+
+            _context.JoinEvent.Remove(join);
+            _context.SaveChanges();
+        }
     }
 }

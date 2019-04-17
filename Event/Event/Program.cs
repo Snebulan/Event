68a89e7a45edfaee228ac9bdd0532e3feb4acee5
Fredrik Gustasfsson <fredrik@vagel.se>
2019-04-17 18:00:16 +0200

    CreateEvent()

--- Event/Event/Program.cs
+++ Event/Event/Program.cs
@@ -8,6 +8,7 @@ namespace Event
     class Program
     {
         private static  bool logedIn = false;
+        private static string logedinUserName = "";
         static void Main(string[] args)
         {
             StartMenu();
@@ -16,15 +17,21 @@ namespace Event
 
         private static void LogIn()
         {
+            var userRole = "";
             do
             {
                 var _context = new EventContext();
                 var userName = InputManager.InputString("Ange användarnamn.");
                 var passWord = InputManager.InputString("Ange lösenord.");
-                var thisUser = _context.User.Where(u => u.Passwd == passWord);
-                if (thisUser.Any())
+                if (_context.User.Any(c => c.Name == userName))
                 {
-                    logedIn = true;
+                    var thisUser = _context.User.FirstOrDefault(u => u.Name == userName);
+                    if (thisUser.Name == userName && thisUser.Passwd == passWord)
+                    {
+                        logedIn = true;
+                        logedinUserName = userName;
+                        userRole = thisUser.Role;
+                    }
                 }
                 else
                 {
@@ -32,7 +39,19 @@ namespace Event
                     Console.WriteLine("Felaktig inloggning, prova igen....");
                 }
             } while (!logedIn);
-            Console.WriteLine(logedIn);
+            Console.Clear();
+            Console.WriteLine($"\nVälkommen {logedinUserName}!\n");
+            if (userRole == "admin")
+            {
+                AdminMenu();
+
+            }
+            else
+            {
+                UserMenu();
+            }
+
+
         }
 
         private static void ShowEvents()
@@ -68,6 +87,38 @@ namespace Event
             StartMenu();
         }
 
+        private static void CreateEvent()
+        {
+            var location = new Location();
+            var type = new Models.Type();
+            var eventNamn = InputManager.InputString("Ange namn på Event.");
+            var startDate = InputManager.InputDate("Ange startdatum.");
+            var endDate = InputManager.InputDate("Ange slutdatum.");
+            Console.WriteLine("\n");
+            var locations = location.ListAllLocations();
+            foreach (var item in locations)
+            {
+                Console.WriteLine($"{item.Id}. {item.City}");
+            }
+            var city = InputManager.InputInt("Välj ort för eventet.");
+            Console.WriteLine("\n");
+            var types = type.ListAllTypes();
+            foreach (var item in types)
+            {
+                Console.WriteLine($"{item.Id}. {item.Name}");
+            }
+            var typeOfEvent = InputManager.InputInt("Välj vilken typ av event.");
+            Console.Clear();
+
+            Console.WriteLine(
+                $"Eventets namn: {eventNamn}\n" +
+                $"Start datum för Event: {startDate.ToString("dddd, dd MMMM yyyy ")}\n" +
+                $"Slut datum för Event: {endDate.ToString("dddd, dd MMMM yyyy ")}\n" +
+                $"Ort: {locations.FirstOrDefault(c => c.Id == city).City}\n" +
+                $"Typ: {types.FirstOrDefault(t => t.Id == typeOfEvent).Name}\n"       
+                );
+        }
+
         private static void StartMenu()
         {
             Console.WriteLine("--- Start ---");
@@ -97,6 +148,19 @@ namespace Event
             Console.WriteLine("5. Ta bort Typ");
             Console.WriteLine("6. Lägg till användare");
             Console.WriteLine("7. Ta bort användare");
+            int selectedStartMenuAlternative = InputManager.InputInt("Välj meny alternativ");
+            switch (selectedStartMenuAlternative)
+            {
+                case 1:
+                    CreateEvent();
+                    break;
+                case 2:
+                    LogIn();
+                    break;
+                default:
+                    break;
+            }
+
         }
 
         private static void UserMenu()
